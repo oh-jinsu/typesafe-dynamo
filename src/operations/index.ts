f38428f9e41d43d68ab6f05f89d1c0b27aaccfad
오진수 <ohjinsu@codersproduct.com>
2023-03-03 11:24:25 +0900

    Export mocks

--- src/operations/index.ts
+++ src/operations/index.ts
@@ -1,11 +1,21 @@
 export * from "./get";
 
+export * from "./get_mock";
+
 export * from "./query";
+
+export * from "./query_mock";
 
 export * from "./scan";
 
+export * from "./scan_mock";
+
 export * from "./put";
 
+export * from "./put_mock";
+
 export * from "./update";
+
+export * from "./update_mock";
 
 export * from "./remove";
